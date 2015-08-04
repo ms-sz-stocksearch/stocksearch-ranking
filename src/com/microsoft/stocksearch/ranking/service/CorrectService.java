b7db71f619aeb53a99533c429f9a930280ab8a26
Junjun Zhao <v-junjzh@microsoft.com>
2015-08-04 20:16:48 +0800

    add interface

--- src/com/microsoft/stocksearch/ranking/service/CorrectService.java
+++ src/com/microsoft/stocksearch/ranking/service/CorrectService.java
@@ -1,5 +1,7 @@
 package com.microsoft.stocksearch.ranking.service;
 
 public abstract class CorrectService {
+	
+	public abstract String[] correct(String query, String[] segments);
 
 }
--- src/com/microsoft/stocksearch/ranking/service/RankService.java
+++ src/com/microsoft/stocksearch/ranking/service/RankService.java
@@ -1,5 +1,9 @@
 package com.microsoft.stocksearch.ranking.service;
 
+import com.microsoft.stocksearch.ranking.beans.QueryResult;
+
 public abstract class RankService {
+	
+	public abstract QueryResult[] sort(String[] keywords);
 	
 }
